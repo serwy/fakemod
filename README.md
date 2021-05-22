0dce3e291c1871983b8e171e34a1aefd592f7304
Roger D. Serwy <roger.serwy@gmail.com>
2021-05-22 12:48:19 -0500

    fakemod redirect to relmod

---
+++ README.md
@@ -0,0 +1,7 @@
+# fakemod
+
+The `fakemod` package has been renamed to `relmod`. 
+It can be found here: https://github.com/serwy/relmod
+
+This stub package will install `relmod`, and any imports
+of `fakemod` will import `relmod` instead and display a warning message.
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from fakemod._version import __version__
+
+with open('README.md', 'rb') as fid:
+    LONG_DESCRIPTION = fid.read().decode('utf8')
+
+setup(
+    name='fakemod',
+    version=__version__,
+    author='Roger D. Serwy',
+    author_email='roger.serwy@gmail.com',
+    license="BSD License",
+    keywords="reload module",
+    url="http://github.com/serwy/relmod",
+    packages=['fakemod'],
+    install_requires=['relmod'],
+    description='Auto-Reloadable Modules and Namespaces',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Testing :: Unit',
+    ],
+)
